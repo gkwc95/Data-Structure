2da01bcba2bcc639d20fe762a73fe448fac5547e
gabrielkho <90828765+gkwc95@users.noreply.github.com>
2022-05-12 22:09:02 +0800

    Update Permutationll.java

--- Permutationll.java
+++ Permutationll.java
@@ -27,5 +27,18 @@ public class Permutationll {
             }
         }
     }
-    
+/*
+	Example {1,1,2}
+	i = 0; we get the first number: 1 and add it in to every position of the remaining number: output: 1 1 2, 1 1 2, 1 2 1
+	Repeat the process and we will get all the permutations.
+	
+	To remove duplicate, we will check if the number added to that position is equal to the number at position - 1.
+	If it is true we will stop the process.
+	
+	For example, we get k = 0, and the remainding numbers in the array is 1 2. We will call the remainder number array, int[] tmp = []{1, 2}
+	i = 0 We add num[k] to the zero position and we get 1 1 2.
+	i = 1 We check num[k] == tmp [i-1], its true does we stop there.
+	
+	https://www.youtube.com/watch?v=K6vqgAEyfng&ab_channel=HuifengGuan <= in chinese
+*/
 }

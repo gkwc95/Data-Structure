ccf031b0b9494fd607dc608071589f3f610c7b82
gabrielkho <e0838413@u.nus.edu>
2022-07-14 21:49:28 +0800

    Create 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.java

---
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder){
+        if (preorder.length == 0 || inorder.length == 0) return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        int mid = -1;
+        for (int i = 0; i < inorder.length; i++){
+            if (inorder[i] == preorder[0]){
+                mid = i;
+                break;
+            }
+        }
+        root.left = buildTree(Arrays.copyOfRange(preorder,1, mid+1), Arrays.copyOfRange(inorder,0,mid));
+        root.right = buildTree(Arrays.copyOfRange(preorder,mid+1,preorder.length), Arrays.copyOfRange(inorder,mid+1,inorder.length));
+        return root;
+    }
+}

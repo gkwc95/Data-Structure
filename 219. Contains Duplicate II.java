d83b0f21badabba666610a7bd1e494d0d1080217
gabrielkho <e0838413@u.nus.edu>
2022-10-21 21:18:39 +0800

    Create 219. Contains Duplicate II.java

---
+++ 219. Contains Duplicate II.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (k == 0) return false;
+        Set<Integer> set = new HashSet<>();
+        for (int  i = 0 ; i < nums.length; i++){
+            if (set.contains(nums[i])) return true;
+            if (i >= k) set.remove(nums[i-k]);
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
